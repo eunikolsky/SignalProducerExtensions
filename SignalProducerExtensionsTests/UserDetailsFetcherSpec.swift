577dcce18f4d47e54847e72ee47efe280a2cccd3
Eugene Nikolsky <eunikolsky@gmail.com>
2020-01-11 16:38:45 +0200

    Add a test for no usernames

--- SignalProducerExtensionsTests/UserDetailsFetcherSpec.swift
+++ SignalProducerExtensionsTests/UserDetailsFetcherSpec.swift
@@ -15,19 +15,27 @@ import Quick
 class UserDetailsFetcherSpec: QuickSpec {
     override func spec() {
         describe("fetchDetails") {
-            it("returns details for all users") {
-                let fakeNetworkRequest = { username in
-                    SignalProducer(value: UserDetails(name: "~" + username))
-                        .delay(0.001, on: QueueScheduler(qos: .userInteractive))
-                }
+            let fakeNetworkRequest = { username in
+                SignalProducer(value: UserDetails(name: "~" + username))
+                    .delay(0.001, on: QueueScheduler(qos: .userInteractive))
+            }
+            let sut = UserDetailsFetcher(networkRequest: fakeNetworkRequest)
 
-                let sut = UserDetailsFetcher(networkRequest: fakeNetworkRequest)
+            it("returns details for all users") {
                 let detailsProducer = sut.fetchDetails(for: ["foo", "bar", "!"])
                 let result = detailsProducer.single()
 
                 let expected = [UserDetails(name: "~foo"), UserDetails(name: "~bar"), UserDetails(name: "~!")]
                 let actual = try! result?.get()
                 expect(actual).to(equal(expected))
+            }
+
+            it("returns empty details for no users") {
+                let detailsProducer = sut.fetchDetails(for: [])
+                let result = detailsProducer.single()
+
+                let actual = try! result?.get()
+                expect(actual).to(beEmpty())
             }
         }
     }
