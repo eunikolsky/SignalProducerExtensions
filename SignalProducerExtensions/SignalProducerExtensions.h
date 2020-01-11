75706512efa2c635e3afc3d4d7a2aa5bddd67b0f
Eugene Nikolsky <eunikolsky@gmail.com>
2020-01-11 12:39:10 +0200

    Make sure template tests run fine

--- SignalProducerExtensions/SignalProducerExtensions.h
+++
@@ -1,8 +0,0 @@
-//! Project version number for SignalProducerExtensions.
-FOUNDATION_EXPORT double SignalProducerExtensionsVersionNumber;
-
-//! Project version string for SignalProducerExtensions.
-FOUNDATION_EXPORT const unsigned char SignalProducerExtensionsVersionString[];
-
-// In this header, you should import all the public headers of your framework using statements like #import <SignalProducerExtensions/PublicHeader.h>
-
---
+++ SignalProducerExtensions/SignalProducerExtensions.swift
@@ -0,0 +1,6 @@
+//
+// Created by u on 11.01.20.
+// Copyright (c) 2020 yes. All rights reserved.
+//
+
+import Foundation
